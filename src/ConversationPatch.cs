b9aba95cc6b9bafb68a3df330688e943ac9dce9f
zalokar <zalokar_mailbox@protonmail.com>
2022-09-08 00:53:38 +0200

    initial commit

---
+++ src/ConversationPatch.cs
@@ -0,0 +1,76 @@
+﻿using HarmonyLib;
+using SandBox.GauntletUI.Map;
+using SandBox.GauntletUI.Missions;
+using System.Collections.Generic;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Conversation;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Map.MapConversation;
+using TaleWorlds.InputSystem;
+using TaleWorlds.Library;
+
+namespace ConversationByNumbers
+{
+    [HarmonyPatch(typeof(MissionGauntletConversationView), nameof(MissionGauntletConversationView.OnMissionScreenTick))]
+    class MissionConversationPatch
+    {
+        static void Postfix(float dt, ref MissionConversationVM ____dataSource)
+        {
+            int numRowKeyPressedCode = Input.GetFirstKeyPressedInRange((int)InputKey.D1);
+            int numPadKeyPressedCode = Input.GetFirstKeyPressedInRange((int)InputKey.Numpad1);
+            int answerIndex = -1;
+
+            if (numRowKeyPressedCode >= (int)InputKey.D1 && numRowKeyPressedCode <= (int)InputKey.D9)
+            {
+                answerIndex = numRowKeyPressedCode - 2;
+            } else if (numPadKeyPressedCode >= (int)InputKey.Numpad1 && numPadKeyPressedCode <= (int)InputKey.Numpad9)
+            {
+                answerIndex = numPadKeyPressedCode - 79;
+            } else
+            {
+                return;
+            }
+
+            var traverse = Traverse.Create(____dataSource);
+            var answerListLength = traverse.Field("_answerList").Property("Count").GetValue<int>();
+            if (answerIndex < 0 || answerIndex >= answerListLength)
+            {
+                return;
+            }
+
+            var OnSelectOption = AccessTools.Method(typeof(MissionConversationVM), "OnSelectOption");
+            OnSelectOption.Invoke(____dataSource, new object[] { answerIndex });
+        }
+    }
+
+    [HarmonyPatch(typeof(GauntletMapConversationView), "Tick")]
+    class MapConversationPatch
+    {
+        static void Postfix(ref MapConversationVM ____dataSource)
+        {
+            int numRowKeyPressedCode = Input.GetFirstKeyPressedInRange((int)InputKey.D1);
+            int numPadKeyPressedCode = Input.GetFirstKeyPressedInRange((int)InputKey.Numpad1);
+            int answerIndex = -1;
+
+            if (numRowKeyPressedCode >= (int)InputKey.D1 && numRowKeyPressedCode <= (int)InputKey.D9)
+            {
+                answerIndex = numRowKeyPressedCode - 2;
+            } else if (numPadKeyPressedCode >= (int)InputKey.Numpad1 && numPadKeyPressedCode <= (int)InputKey.Numpad9)
+            {
+                answerIndex = numPadKeyPressedCode - 79;
+            } else
+            {
+                return;
+            }
+
+            var traverse = Traverse.Create(____dataSource);
+            var dialogController = traverse.Field("_dialogController").GetValue<MissionConversationVM>();
+            var answerListLength = dialogController.AnswerList.Count;
+            if (answerIndex < 0 || answerIndex >= answerListLength)
+            {
+                return;
+            }
+
+            var OnSelectOption = AccessTools.Method(typeof(MissionConversationVM), "OnSelectOption");
+            OnSelectOption.Invoke(dialogController, new object[] { answerIndex });
+        }
+    }
+}
---
+++ src/SubModule.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace ConversationByNumbers
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Harmony harmony = new Harmony("conversation_by_numbers");
+            harmony.PatchAll();
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
