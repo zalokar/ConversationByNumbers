e8dff122f587e1fc93e5893a4ee8ebecd3f2965d
zalokar <zalokar_mailbox@protonmail.com>
2022-09-10 16:42:40 +0200

    check if option is disabled

--- src/ConversationPatch.cs
+++ src/ConversationPatch.cs
@@ -1,11 +1,15 @@
 ﻿using HarmonyLib;
 using SandBox.GauntletUI.Map;
 using SandBox.GauntletUI.Missions;
+using System;
 using System.Collections.Generic;
+using System.Reflection.Emit;
+using TaleWorlds.CampaignSystem.Conversation;
 using TaleWorlds.CampaignSystem.ViewModelCollection.Conversation;
 using TaleWorlds.CampaignSystem.ViewModelCollection.Map.MapConversation;
 using TaleWorlds.InputSystem;
 using TaleWorlds.Library;
+using TaleWorlds.Localization;
 
 namespace ConversationByNumbers
 {
@@ -29,13 +33,18 @@ namespace ConversationByNumbers
                 return;
             }
 
-            var traverse = Traverse.Create(____dataSource);
-            var answerListLength = traverse.Field("_answerList").Property("Count").GetValue<int>();
+            var dataSource = Traverse.Create(____dataSource).GetValue<MissionConversationVM>();
+            var answerListLength = dataSource.AnswerList.Count;
             if (answerIndex < 0 || answerIndex >= answerListLength)
             {
                 return;
             }
 
+            if (!dataSource.AnswerList[answerIndex].IsEnabled)
+            {
+                return;
+            }
+            
             var OnSelectOption = AccessTools.Method(typeof(MissionConversationVM), "OnSelectOption");
             OnSelectOption.Invoke(____dataSource, new object[] { answerIndex });
         }
@@ -69,8 +78,67 @@ namespace ConversationByNumbers
                 return;
             }
 
+            if (!dialogController.AnswerList[answerIndex].IsEnabled)
+            {
+                return;
+            }
+
             var OnSelectOption = AccessTools.Method(typeof(MissionConversationVM), "OnSelectOption");
             OnSelectOption.Invoke(dialogController, new object[] { answerIndex });
+        }
+    }
+
+    [HarmonyPatch(typeof(MissionConversationVM), "Refresh")]
+    class NumbersToAnswersPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var setForceCameraToTurnToPlayer = AccessTools.Method(typeof(ConversationManager), "set_ForceCameraToTurnToPlayer");
+            var foundInstructionBeforeLoop = false;
+            var foundInstructionInsideLoop = false;
+
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Callvirt && instruction.operand == (object)setForceCameraToTurnToPlayer)
+                {
+                    foundInstructionBeforeLoop = true;
+                }
+
+                if (!foundInstructionInsideLoop && foundInstructionBeforeLoop && instruction.opcode == OpCodes.Ldarg_0)
+                {
+                    // X = i + ". "
+                    yield return new CodeInstruction(OpCodes.Ldloc, 6);
+                    yield return new CodeInstruction(OpCodes.Ldstr, ". ");
+                    yield return new CodeInstruction(OpCodes.Box, typeof(Int32));
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(string), nameof(String.Concat), parameters: new Type[] { typeof(object), typeof(object) }));
+
+                    // X += this._conversationManager.CurOptions[i].Text.ToString()
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(MissionConversationVM), "_conversationManager"));
+                    yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(ConversationManager), "get_CurOptions"));
+                    yield return new CodeInstruction(OpCodes.Ldloc, 6);
+                    yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(List<ConversationSentenceOption>), "get_Item"));
+                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(ConversationSentenceOption), nameof(ConversationSentenceOption.Text)));
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(TextObject), nameof(TextObject.ToString)));
+                    yield return new CodeInstruction(OpCodes.Box, typeof(Int32));
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(String), nameof(String.Concat), parameters: new Type[] { typeof(object), typeof(object) }));
+
+                    // this._conversationManager.CurOptions[i].Text = X
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(MissionConversationVM), "_conversationManager"));
+                    yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(ConversationManager), "get_CurOptions"));
+                    yield return new CodeInstruction(OpCodes.Ldloc, 6);
+                    yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(List<ConversationSentenceOption>), "get_Item"));
+                    yield return new CodeInstruction(OpCodes.Stfld, AccessTools.Field(typeof(ConversationSentenceOption), nameof(ConversationSentenceOption.Text)));
+                    foundInstructionInsideLoop = true;
+                }
+                yield return instruction;
+            }
+
+            if (foundInstructionBeforeLoop is false)
+            {
+                throw new ArgumentException("Cannot find ConversationManager::set_ForceCameraToTurnToPlayer in Conversation.MissionConversationVM");
+            }
         }
     }
 }
